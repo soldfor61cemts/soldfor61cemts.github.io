c0555f0fe6abfe2e182adfe785aa61e2bc7e2a98
void <hardik.mahendru.hm@gmail.com>
2024-08-31 22:15:16 +0530

    client

---
+++ server.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('playerMove', (data) => {
+        socket.broadcast.emit('playerMove', data);
+    });
+
+    socket.on('playerShoot', (data) => {
+        socket.broadcast.emit('playerShoot', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
