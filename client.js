2bb7d863a6b24f460e0195bcc992f96d742fe0d2
void <hardik.mahendru.hm@gmail.com>
2024-08-31 22:22:04 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,6 +1,5 @@
 
-const socket = io();
-
+const socket = io('https://soldfor61cemts-github-io.onrender.com/')
 // Initialize Three.js scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
